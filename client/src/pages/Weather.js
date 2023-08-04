ff375a782b4f7ef36ab03645d08a2192e9b9fdd9
Eric Davidson <edavidson1410@gmail.com>
2023-08-04 17:28:15 -0500

    Weather API accessed, shown, and user input added.

--- client/src/pages/Weather.js
+++ client/src/pages/Weather.js
@@ -1,27 +1,80 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import "./Weather.css";
 
 
 export default function Weather() {
 
-    const [city, setCity] = useState("Unknown")
+    let error = false;
+    const errorDiv = document.getElementById("error");
 
-    function displayData(api) {
+    let input;
+    const [zipCode, setZipCode] = useState("63109");
+    useEffect(() => {
+            
+            input = document.getElementById("userInput");
+            console.log(zipCode)
+    })
+
+    const [city, setCity] = useState("Unknown");
+    const [region, setRegion] = useState("Unknown");
+    const [country, setCountry] = useState("Unknown");
+
+    const [temperature, setTemperature] = useState("Unknown");
+    const [condition, setCondition] = useState("Unknown");
+    const [icon, setIcon] = useState("Unknown");
+
+    function displayCity(api) {
         const cityName = api.location.name;
+        const regionName = api.location.region;
+        const countryName = api.location.country;
         setCity(cityName);
+        setRegion(regionName);
+        setCountry(countryName);
+    };
 
+    function displayWeather(api) {
+        const temp = api.current.temp_f;
+        const conditionType = api.current.condition.text;
+        const weatherIcon = api.current.condition.icon;
+        setTemperature(temp);
+        setCondition(conditionType);
+        setIcon(weatherIcon);
+    };
+
+    function errorMessage() {
+        if(error) {
+            errorDiv.innerHTML = "Please enter a valid zipcode."
+            error = false;
+        } else errorDiv.innerHTML = "";
     }
 
-    fetch("http://api.weatherapi.com/v1/current.json?key=1dd34d02c3a94e5285903838230408&q=63109&aqi=no").then(function(response) {
-        return response.json();
-    }).then(function(response) {
-        console.log(response)
-        displayData(response)
+    fetch(`http://api.weatherapi.com/v1/current.json?key=1dd34d02c3a94e5285903838230408&q=${zipCode}&aqi=no`).then(response => {
+        if(response.ok) return response.json()
+        else { throw new Error;}
+    }).then(response => {
+        errorMessage();
+        displayCity(response)
+        displayWeather(response)
+    }).catch(() => {
+        error = true;
+        errorMessage()
     });
-
 
     return(
         <div className="weatherApp">
-            <div id="city">{city}</div>
+            <input type="text" maxlenght="5" id="userInput"></input>
+            <button onClick={() => setZipCode(input.value)}>Submit</button>
+            <div id="error"></div>
+            <div className="location">
+                <div id="city">{city}</div>
+                <div id="region">{region}</div>
+                <div id="country">{country}</div>
+            </div>
+            <div className="weather">
+                <div id="temperature">{temperature} <span>&#8457;</span></div>
+                <div id="condition">{condition}</div>
+                <img src={icon}/>
+            </div>
         </div>
     )
 };
