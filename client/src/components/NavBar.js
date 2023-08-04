a1059e5c36c67488a3f32293479a54776fdae629
Eric Davidson <edavidson1410@gmail.com>
2023-08-03 19:03:51 -0500

    added NavBar. needs Style. Create Weather app.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,22 @@
-import "./NavBar.css"
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import logo from "../imgs/reactLogo.png"
+
+const links = ["", "counter", "weather"]
 
 export const NavBar = () => {
 
     return (
         <div className="navBar">
-            <ul className="links">
-                <li>Homepage</li>
-                <li>Counter</li>
-                <li>Weather</li>
-            </ul>
+            {/* <img src={logo} alt="react Logo" />
+            {links.map((link) => (
+                <Link key={link} to="/">{link}</Link>
+            ))} */}
+            
+            <img src={logo} alt="react Logo" />
+            <Link to="/">Home</Link>
+            <Link to="/counter">Counter</Link>
+            <Link to="/weather">Weather</Link>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import './index.css';
 import HomePage from './pages/HomePage';
 import Counter from './pages/Counter';
@@ -9,23 +9,31 @@ import { NavBar } from './components/NavBar';
 
 const router = createBrowserRouter([
   {
-      path: "/",
-      element: <HomePage />,
-  },
-  {
-      path: "/counter",
-      element: <Counter />,
-  },
-  {
-    path: "/weather",
-    element: <Weather />
-  }
+    element: (
+      <>
+        <NavBar />
+        <Outlet />
+      </>
+    ),
+    children: [
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/counter",
+        element: <Counter />,
+    },
+    {
+      path: "/weather",
+      element: <Weather />
+    }
+  ]}
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
